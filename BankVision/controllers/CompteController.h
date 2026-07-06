38d23f46122618f9c8262fb86d1c8f26f31e8bb7
Adama-Faye-tech <dapy585@gmail.com>
2026-07-06 22:17:32 +0100

    Delete BankVision directory

--- BankVision/controllers/CompteController.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CompteController {
-public:
-    CompteController();
-    bool depot(int compteId, double montant);
-    bool retrait(int compteId, double montant);
-    bool transfert(int fromId, int toId, double montant);
-};
